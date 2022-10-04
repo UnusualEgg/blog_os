c03fbdd49fc803d429bc1f49dcf44d3f5f4961d6
UnusualEgg <shotgun282006@gmail.com>
2022-10-04 16:28:13 -0500

    test

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -94,7 +94,7 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     let mut keyboard = KEYBOARD.lock();
     let mut port = Port::new(0x60);
 
-    let scancode: u8 = unsafe { port.read() };
+    let scancode: u8 = unsafe { port.read() };//wow
     if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
         if let Some(key) = keyboard.process_keyevent(key_event) {
             // use crate::vga_buffer::Writer;
